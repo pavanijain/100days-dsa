037836700a732a201d97124e2ea5a90931c44c9e
pavanijain <pavanijain12@gmail.com>
2026-02-04 22:06:38 +0530

    upload

---
+++ day004.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+int n,i;
+scanf("%d",&n);
+int arr[n];
+for(i=0;i<n;i++){
+scanf("%d\n",&arr[i]);
+
+}
+int left=0,right=n-1,temp;
+while(left<right){
+    temp = arr[left];
+    arr[left] = arr[right];
+    arr[right]=temp;
+
+    left++;
+    right--;
+}
+for(int i = 0;i<n;i++){
+    printf("%d ",arr[i]);
+}
+    return 0;
+}
+
+

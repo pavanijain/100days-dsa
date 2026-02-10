9f0e97a5b1251ac89a38590702eeb91e576b942f
pavanijain <pavanijain12@gmail.com>
2026-02-10 23:48:38 +0530

    upload

--- day009.c
+++ day009.c
@@ -2,12 +2,11 @@
 #include <string.h>
 
 int main() {
-    char code_name[100];   // buffer for input string
-    scanf("%s", code_name); // read string (no spaces)
-
+    char code_name[100];   
+    scanf("%s", code_name); 
     int len = strlen(code_name);
 
-    // print characters in reverse order
+    
     for (int i = len - 1; i >= 0; i--) {
         printf("%c", code_name[i]);
     }

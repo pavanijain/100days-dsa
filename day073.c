9b7b556a9154963c1c9182a08f0f3d4b035cd1eb
pavanijain <pavanijain12@gmail.com>
2026-04-14 16:33:59 +0530

    upload

---
+++ day073.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[1000];
+    
+    // Input
+    printf("Enter the string: ");
+    scanf("%s", s);
+
+    int freq[26] = {0};
+
+    // Step 1: Count frequency
+    for (int i = 0; i < strlen(s); i++) {
+        freq[s[i] - 'a']++;
+    }
+
+    // Step 2: Find first non-repeating character
+    for (int i = 0; i < strlen(s); i++) {
+        if (freq[s[i] - 'a'] == 1) {
+            printf("%c\n", s[i]);
+            return 0;
+        }
+    }
+
+    // Step 3: If none found
+    printf("$\n");
+
+    return 0;
+}
